9f2199c9e2716cf42b03011896fd050c54c71ab6
peterclermy <peterclermy232@gmail.com>
2022-07-13 08:58:26 +0300

    added content in about

---
+++ .history/src/components/About_20220713085748.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { image } from "../data/data";
+
+function About() {
+  return (
+     <div id="about">
+      <h2>About Me</h2>
+      <p>This is About me</p>
+      //<img src={image} alt="I made this" />
+     </div>
+  );
+}
+
+export default About;
